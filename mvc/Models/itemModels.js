dd0b5108589a8d43167661c517d85a13fae25b71
Miel Velazquez <mielvelazquezz@gmail.com>
2023-09-27 15:35:26 -0300

    first commit

---
+++ mvc/Models/itemModels.js
@@ -0,0 +1,22 @@
+const Item = {
+
+    name:"",
+    description:"",
+    seeInfo:function(){
+
+        return `Name: ${this.name} Description: ${this.description}`
+
+    },
+
+    toJSON: function(){
+
+        return{
+        "name":this.name,
+        "description":this.description
+        }
+
+    }
+
+}
+
+module.exports = Item;
